e700bee59472b28d1ee15da50796431433344bbb
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-06-23 11:32:10 -0300

    deploy actualizado

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['robohash.org']
+        domains:['robohash.org','static.dafiti.com.ar','storage.googleapis.com','www.google.com']
     }
 }
 
