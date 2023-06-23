c683cefdf501dfe5bf3030add1a7c9a788920e68
Santiago Leyria lino <121639887+SantiagoLeyriaLino@users.noreply.github.com>
2023-06-22 22:46:42 -0300

    Delete app/products copy directory

--- app/products copy/children/shoes/[id]/page.jsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import ProductDetail from "@/components/prdouctDetail"
-
-export default function DetailChildrenShoes() {
-
-    return (
-        <ProductDetail />
-    )
-}
--- app/products copy/female/t-shirt/[id]/page.jsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import ProductDetail from "@/components/prdouctDetail"
-
-export default function DetailFemaleTshirt() {
-
-    return (
-        <ProductDetail />
-    )
-}
