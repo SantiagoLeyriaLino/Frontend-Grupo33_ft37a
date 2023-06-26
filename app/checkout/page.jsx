32cff510211225f0d99a8b495750e8edfcf58a33
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-06-26 04:05:12 -0300

    deploy

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -11,100 +11,80 @@ import CheckoutForm from "@/components/Stripe Components/CheckoutForm";
 const stripePromise = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_KEY_PUBLIC}`); // estado "products"
 
 export default function CheckoutPage() {
+    if (typeof localStorage !== 'undefined') {
+        const [products, setProducts] = useState();
+        const [clientSecret, setClientSecret] = useState("");
+        const amount = useSelector(state => state.products.totalPay);
 
-    // const options = {
-    //     // passing the client secret obtained in step 3
-    //     clientSecret: '{{CLIENT_SECRET}}',
-    //     // Fully customizable with appearance API.
-    //     appearance: {/*...*/},
-    //   };
-
-    if (typeof localStorage !== 'undefined'){ {const [products, setProducts] = useState()
-    const [clientSecret, setClientSecret] = useState("");
-    // const [stripePromise, setStripePromise] = useState("");
-    const amount = useSelector(state => state.products.totalPay)
-
-    // setStripePromise(loadStripe('pk_test_51NLR6uLcLCj2IYBvkClOWECjiBqzYZeLetrIKGc9QlEeC2tJQKMq12enElgMR9lJzingQYbAq09DR5Z8XmjRpEwp00xG9VyEUi'))
-
-    const getMyCart = () => { // extrae localStorage y almacena en estado "products"
-        const myCartLocal = localStorage.getItem("myCart")
-        const myCart = JSON.parse(myCartLocal)
-        console.log(myCart);
-        setProducts(myCart)
-    }
-
-    const handleCantChange = (producId, newCant) => {
-        console.log('aca estoy');
-        const newCantProduct = products.map((product) => {
-
-            if (product._id === producId) {
-                console.log('lo encontre');
-                return { ...product, cant: newCant };
-            }
-            return product
-        })
-        setProducts(newCantProduct)
-    }
-
-    const handleDeleteProductCart = (producId) => {
-        console.log('voy a eliminar');
-        const newArrayProduct = products.filter(product => {
-            if (product._id !== producId) {
-                return product
-            }
-        })
-        setProducts(newArrayProduct)
-    }
-
-    useEffect(() => {
-        getMyCart()
-        console.log(products);
-    }, [])
-
-    useEffect(() => {
-        localStorage.setItem('myCart', JSON.stringify(products))
-    }, [products])
-
-    const getClientSecret = async (amount) => {
-        console.log(amount);
-        try {
-            const {data} = await axios.post('https://backend-grupo-33ft37a-jpaguo1zy-santiagoleyrialino.vercel.app/payment', { amount })
-            setClientSecret(data.data)
-        } catch (error) {
-        console.log(error); 
+        const getMyCart = () => {
+            const myCartLocal = localStorage.getItem("myCart");
+            const myCart = JSON.parse(myCartLocal);
+            console.log(myCart);
+            setProducts(myCart);
         }
-    }
 
-
-    useEffect(() => {
-        if (amount > 0) {
-            getClientSecret(amount)
-        }
-    }, [amount])
-    
-
-    return (
-        <main>
-
-            <section className="pt-[10rem] pb-[4rem] flex w-[80%] mx-[auto] justify-between min-h-[100vh]">
-                <div className="w-[60%] pt-[1rem]">
-                {
-                    stripePromise && clientSecret && (
-                        <Elements stripe={stripePromise} options={{clientSecret}} >
-                            {/* <CheckoutForm products={products}/> */}
-                            <CheckoutForm />
-                            {/* <CardElement className="w-[600px] h-[fit-content]"/> */}
-                        </Elements>
-                    )
+        const handleCantChange = (producId, newCant) => {
+            console.log('aca estoy');
+            const newCantProduct = products.map((product) => {
+                if (product._id === producId) {
+                    console.log('lo encontre');
+                    return { ...product, cant: newCant };
                 }
-                </div>
-                <div className="w-[40%] pt-[1rem]">
-                <Summary products={products}
-                    handleCantChange={handleCantChange}
-                    handleDeleteProductCart={handleDeleteProductCart}
-                />
-                </div>
-            </section>
-        </main>
-    )}
+                return product;
+            });
+            setProducts(newCantProduct);
+        }
+
+        const handleDeleteProductCart = (producId) => {
+            console.log('voy a eliminar');
+            const newArrayProduct = products.filter(product => product._id !== producId);
+            setProducts(newArrayProduct);
+        }
+
+        useEffect(() => {
+            getMyCart();
+            console.log(products);
+        }, []);
+
+        useEffect(() => {
+            localStorage.setItem('myCart', JSON.stringify(products));
+        }, [products]);
+
+        const getClientSecret = async (amount) => {
+            console.log(amount);
+            try {
+                const { data } = await axios.post('https://backend-grupo-33ft37a-jpaguo1zy-santiagoleyrialino.vercel.app/payment', { amount });
+                setClientSecret(data.data);
+            } catch (error) {
+                console.log(error); 
+            }
+        }
+
+        useEffect(() => {
+            if (amount > 0) {
+                getClientSecret(amount);
+            }
+        }, [amount]);
+
+        return (
+            <main>
+                <section className="pt-[10rem] pb-[4rem] flex w-[80%] mx-[auto] justify-between min-h-[100vh]">
+                    <div className="w-[60%] pt-[1rem]">
+                        {stripePromise && clientSecret && (
+                            <Elements stripe={stripePromise} options={{ clientSecret }}>
+                                <CheckoutForm />
+                            </Elements>
+                        )}
+                    </div>
+                    <div className="w-[40%] pt-[1rem]">
+                        <Summary
+                            products={products}
+                            handleCantChange={handleCantChange}
+                            handleDeleteProductCart={handleDeleteProductCart}
+                        />
+                    </div>
+                </section>
+            </main>
+        );
+    }
 }
--- app/purchaseHistory/page.jsx
+++ app/purchaseHistory/page.jsx
@@ -3,74 +3,77 @@ import { useState, useEffect } from "react"
 import CardHistoryPurchase from "@/components/CardHistoryPurchase"
 import axios from "axios"
 
-export default function PurchaseHistory(){
-    if (typeof localStorage !== 'undefined'){{ const user = JSON.parse(localStorage.getItem('user'))
-    console.log(user)
-
-    const [getUser, setGetUser] = useState();
-    const [localUser, setLocalUser] = useState();
-    
-    useEffect(()=>{
-        const userId = user.data._id
-        axios.get(`https://backend-grupo-33ft37a-jpaguo1zy-santiagoleyrialino.vercel.app/users/${userId}`)
-        .then((response)=>setGetUser(response.data))
-    },[])
-
-    useEffect(()=>{
-        const user = JSON.parse(localStorage.getItem('user'))
-        console.log(user)
+export default function PurchaseHistory() {
+    if (typeof localStorage !== 'undefined') {
+      const user = JSON.parse(localStorage.getItem('user'));
+      console.log(user);
+  
+      const [getUser, setGetUser] = useState();
+      const [localUser, setLocalUser] = useState();
+  
+      useEffect(() => {
+        const userId = user.data._id;
+        axios
+          .get(`https://backend-grupo-33ft37a-jpaguo1zy-santiagoleyrialino.vercel.app/users/${userId}`)
+          .then((response) => setGetUser(response.data));
+      }, []);
+  
+      useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'));
+        console.log(user);
         user.data = getUser;
         localStorage.setItem('user', JSON.stringify(user));
-        console.log({ESTEESELUSUARIOKAPO:getUser})
-        setLocalUser(user)
-    },[getUser])
-
-    return( 
+        console.log({ ESTEESELUSUARIOKAPO: getUser });
+        setLocalUser(user);
+      }, [getUser]);
+  
+      return (
         <main className="flex flex-col pt-[9rem] min-h-[100vh] bg-gray-100 p-8 rounded-lg shadow-md w-full max-w-3xl mx-auto">
-        {localUser && localUser.data && localUser.data.purchaseHistory ? (
-            user.data.purchaseHistory.map((purchase, index) => {
-            return (
+          {localUser && localUser.data && localUser.data.purchaseHistory ? (
+            localUser.data.purchaseHistory.map((purchase, index) => {
+              return (
                 <div key={index}>
-                <hr className="border-1 border-black" />
-                <div>
+                  <hr className="border-1 border-black" />
+                  <div>
                     <p>Date of purchase: {purchase.date}</p>
                     <p>Total amount: {purchase.amount}</p>
                     <hr />
                     <br className="w-full" />
-                </div>
-                <div className="flex">
+                  </div>
+                  <div className="flex">
                     {purchase.products.map((product, index) => {
-                    if (index === 0) {
+                      if (index === 0) {
                         return (
-                        <div key={index} className="ml-auto">
+                          <div key={index} className="ml-auto">
                             <CardHistoryPurchase
-                            product={product.productId}
-                            size={product.size}
-                            cant={product.cant}
+                              product={product.productId}
+                              size={product.size}
+                              cant={product.cant}
                             />
-                        </div>
+                          </div>
                         );
-                    } else {
+                      } else {
                         return (
-                        <div key={index}>
+                          <div key={index}>
                             <CardHistoryPurchase
-                            product={product.productId}
-                            size={product.size}
-                            cant={product.cant}
+                              product={product.productId}
+                              size={product.size}
+                              cant={product.cant}
                             />
-                        </div>
+                          </div>
                         );
-                    }
+                      }
                     })}
+                  </div>
+                  <hr className="border-1 border-black my-1" />
+                  <br className="w-full" />
                 </div>
-                <hr className="border-1 border-black my-1" />
-                <br className="w-full" />
-                </div>
-            );
+              );
             })
-        ) : (
+          ) : (
             <span>loading...</span>
-        )}
+          )}
         </main>
-    )}
-}
+      );
+    }
+  }
