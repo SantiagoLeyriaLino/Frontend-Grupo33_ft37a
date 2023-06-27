ac313b264d55b9de556522dbb45fd6a81caa01af
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-06-27 17:47:49 -0300

    deploy

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -10,8 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 export default function ProductCard({ product }) {
 
-    if (typeof localStorage !== 'undefined'){
-        const notify = (message) => {
+    const notify = (message) => {
         toast.success(message, {
             autoClose: 2000,
         });
@@ -26,6 +25,7 @@ export default function ProductCard({ product }) {
     const [toolTip, setTooltip] = useState({})
     const [cant, setCant] = useState(1)
     const [priceModify, setPriceModify] = useState(product.price)
+    const [sizeCheck, setSizeCheck] = useState([])
 
     // const [productos, setProductos] = useState([])
 
@@ -47,12 +47,12 @@ export default function ProductCard({ product }) {
 
     const addMyCart = () => {
         console.log(product.cant);
-        const myCartLocal = localStorage.getItem('myCart')
+        const myCartLocal = window.localStorage.getItem('myCart')||""
         const myCartParse = JSON.parse(myCartLocal)
         const productFind = myCartParse.find(prod => (prod._id === product._id) && (prod.cant === cant))
         if (!productFind) {
             if (myCartParse.length === 0) {
-                 localStorage.setItem('myCart', JSON.stringify([{ ...product, cant: cant }]))
+                window.localStorage.setItem('myCart', JSON.stringify([{ ...product, cant: cant }]))
                 notify('Add to Cart')
             } else {
                 const productMyCart = { ...product, cant: cant }
@@ -60,14 +60,14 @@ export default function ProductCard({ product }) {
                 if (!sameProduct) {
                     const myCart = [...myCartParse, productMyCart]
                     console.log(myCart);
-                   localStorage.setItem('myCart', JSON.stringify(myCart))
+                    window.localStorage.setItem('myCart', JSON.stringify(myCart))
                     notify('Add to Cart')
                 } else {
 
                     console.log(sameProduct);
                     const arrayFilter = myCartParse.filter(prod => prod._id !== sameProduct._id)
                     const newArray = [...arrayFilter, productMyCart]
-                     localStorage.setItem('myCart', JSON.stringify(newArray))
+                    window.localStorage.setItem('myCart', JSON.stringify(newArray))
                     notify('Add to Cart')
                 }
             }
@@ -89,10 +89,22 @@ export default function ProductCard({ product }) {
 
     }
 
+    const handleCheckSize = (event) => {
+        const { checked, name } = event.target
+        const index = parseInt(name)
+        if (checked) {
+            setSizeCheck([...sizeCheck, index]);
+        } else {
+            const newArray = sizeCheck.filter(size => size !== index)
+            setSizeCheck(newArray);
+        }
+    }
+    console.log(sizeCheck);
+
     useEffect(() => {
 
         // setTooltip(product)
-    }, [cant])
+    }, [cant, sizeCheck])
 
     // const handlePrice = () =>{
 
@@ -108,37 +120,37 @@ export default function ProductCard({ product }) {
                 <div className="flex gap-x-[1rem] py-[1rem] z-0">
                     {
                         (product?.sameCode?.length > 0)
-                        ?
-                        <div className="w-[20%] flex flex-col gap-y-[1rem]">
-                        {
-                            product?.sameCode?.map((color, index) => {
-                                if (product?._id !== color?._id) {
-                                    return (
-                                        <Image
-                                            key={index}
-                                            src={color?.images[0]} alt={color?.name} width={200} height={400} />
-                                    )
+                            ?
+                            <div className="w-[20%] flex flex-col gap-y-[1rem]">
+                                {
+                                    product?.sameCode?.map((color, index) => {
+                                        if (product?._id !== color?._id) {
+                                            return (
+                                                <Image
+                                                    key={index}
+                                                    src={color?.images[0]} alt={color?.name} width={200} height={400} />
+                                            )
+                                        }
+                                    })
                                 }
-                            })
-                        }
-                    </div>
-                    :
-                    <></>
+                            </div>
+                            :
+                            <></>
                     }
-                    
-                    <div className={`flex flex-col gap-y-[1rem] ${(product.sameCode.length > 0) ?'w-[70%]' : 'w-[90%] mx-[auto]'}`}>
+
+                    <div className={`flex flex-col gap-y-[1rem] ${(product.sameCode.length > 0) ? 'w-[70%]' : 'w-[90%] mx-[auto]'}`}>
                         <div className="relative">
-                        <Image
-                            className="w-[100%] cursor-pointer"
-                            src={product?.images[0]} alt={product?.name} width={200} height={400} />
+                            <Image
+                                className="w-[100%] cursor-pointer"
+                                src={product?.images[0]} alt={product?.name} width={200} height={400} />
                             {
-                        (product?.stock === 0) 
-                        ?
-                        <Image src={sold} alt="sold-out" width={200} height={200} className="absolute top-[10px] left-[10px] w-[45%]" />
-                        :
-                        <></>
-                    }
-                    </div>
+                                (product?.stock === 0)
+                                    ?
+                                    <Image src={sold} alt="sold-out" width={200} height={200} className="absolute top-[10px] left-[10px] w-[45%]" />
+                                    :
+                                    <></>
+                            }
+                        </div>
 
                         <div className="w-[90%] mx-[auto] flex flex-col gap-y-[0.6rem]" >
                             <h2 className="font-bold">{product?.brand}</h2>
@@ -167,19 +179,48 @@ export default function ProductCard({ product }) {
                             <span
 
                                 className="font-bold text-[#F8652A]">$ {product.price * cant}</span>
-                            {/* <select
-                                name="" id="" className="text-black p-[0.6rem] rounded-[0.6rem]">
-                                <option value="" disabled>Elegir talle</option>
-                                
-                                
-                                
-                            </select> */}
                             {
-                                (product?.stock > 0)
-                                ?
-                                <span
-                                    onClick={addMyCart}
-                                    className="cursor-pointer w-[100%] py-[0.6rem] text-center bg-[white] text-black rounded-[0.6rem] hover:bg-[#F8652A] hover:text-white">Añadir a carrito</span>
+                                product?.stock > 0
+                                    ?
+                                    <div className="flex flex-col gap-y-[0.2rem]">
+                                        Size
+                                        {
+                                            product?.size?.map((size, index) => {
+                                                return (
+                                                    <div key={index} className="flex items-center gap-x-[0.4rem]">
+                                                        <input onChange={handleCheckSize} type="checkbox" name={index} id="" />
+                                                        <span className="w-[16px]">{size.size}</span>
+
+                                                        <div className={`${sizeCheck.includes(index) ? "opacity-1" : "opacity-0"} flex h-[fit-content] gap-x-[0.4rem]`}>
+                                                            <span>Cant</span>
+                                                            <div className="flex">
+                                                                <div
+                                                                    className="flex items-center justify-center bg-[#FA8B61] hover:bg-[#F8652A] px-[0.2rem] rounded-l-[0.6rem]  cursor-pointer"
+                                                                    onClick={resCant}><span className="text-[0.7rem]">{`<`}</span></div>
+                                                                <div className="flex items-center justify-center w-[20px] bg-white text-center  text-black ">
+                                                                    <span className="text-[0.7rem]">{cant}</span>
+                                                                </div>
+                                                                <div
+                                                                    className="flex items-center justify-center bg-[#FA8B61] hover:bg-[#F8652A] px-[0.2rem] rounded-r-[0.6rem] cursor-pointer"
+                                                                    onClick={sumCant}><span className="text-[0.7rem]">{`>`}</span></div>
+                                                            </div>
+                                                        </div>
+
+                                                    </div>
+
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    :
+                                    <></>
+                            }
+                            {
+                                ((product?.stock > 0) && (sizeCheck.length > 0))
+                                    ?
+                                    <span
+                                        onClick={addMyCart}
+                                        className="cursor-pointer w-[100%] py-[0.6rem] text-center bg-[white] text-black rounded-[0.6rem] hover:bg-[#F8652A] hover:text-white">Añadir a carrito</span>
                                     :
                                     <></>
                             }
@@ -190,21 +231,21 @@ export default function ProductCard({ product }) {
         >
             <div
                 onClick={() => { handleOnClick(product?._id) }}
-                className="w-[30%] flex flex-col gap-y-[1rem]"
+                className="w-[28%]  flex flex-col gap-y-[0.6rem] h-[340px] "
             >
-                <div className="relative">
-                <Image
-                    className="w-[90%] mx-[auto] cursor-pointer"
-                    src={product?.images[0]} alt={product?.name} width={200} height={400} />
+                <div className="relative h-[70%] ">
+                    <Image
+                        className="w-[90%] h-[100%] object-cover mx-[auto] cursor-pointer"
+                        src={product?.images[0]} alt={product?.name} width={200} height={400} />
                     {
-                        (product?.stock === 0) 
-                        ?
-                        <Image src={sold} alt="sold-out" width={200} height={200} className="absolute top-[10px] left-[15px] w-[45%]" />
-                        :
-                        <></>
+                        (product?.stock === 0)
+                            ?
+                            <Image src={sold} alt="sold-out" width={200} height={200} className="absolute top-[10px] left-[15px] w-[45%]" />
+                            :
+                            <></>
                     }
                 </div>
-                <div className="w-[90%] mx-[auto]">
+                <div className="w-[90%] mx-[auto] h-[30%]">
                     <h2 className="font-bold">{product?.brand}</h2>
                     <p>{product?.name}</p>
                     <span className="font-bold">$ {product.price}</span>
@@ -215,5 +256,5 @@ export default function ProductCard({ product }) {
                     theme="light" />
             </div>
         </Tippy>
-    )}
+    )
 }
