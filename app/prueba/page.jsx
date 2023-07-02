082a988306c8681fe4a4ad05ddaa1c0453d8e1af
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-07-02 20:33:41 -0300

    deploy

--- app/prueba/page.jsx
+++ app/prueba/page.jsx
@@ -1,12 +1,12 @@
-'use client'
-import CarouselHome from "@/components/Carousel/CarouselHome"
-import StripeCard from "@/components/Stripe Components/StripeCard"
+// 'use client'
+// import CarouselHome from "@/components/Carousel/CarouselHome"
+// import StripeCard from "@/components/Stripe Components/StripeCard"
 
-export default function pruebaPage(){
-    return (
-        <div className="pt-[10rem]">
-        {/* <CarouselHome/> */}
-        <StripeCard/>
-        </div>
-    )
-}
+// export default function pruebaPage(){
+//     return (
+//         <div className="pt-[10rem]">
+//         {/* <CarouselHome/> */}
+//         <StripeCard/>
+//         </div>
+//     )
+// }
