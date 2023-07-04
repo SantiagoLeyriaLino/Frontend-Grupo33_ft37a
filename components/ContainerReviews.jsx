c1ca43faea0d09855a3020df3f18641572494741
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-07-04 03:46:02 -0300

    deploy

--- components/ContainerReviews.jsx
+++ components/ContainerReviews.jsx
@@ -51,7 +51,7 @@ export default function ContainerReviews({ reviews, productId, setRefresh}){
         }
         {reviews.length ?
             <div className="flex flex-col gap-y-[0.8rem] content-center">
-                {reviews.map((review)=> <Review data={review}/>)}
+                {reviews.map((review, index)=> <Review key={index} data={review}/>)}
             </div> : <></>
         }
     </div>
