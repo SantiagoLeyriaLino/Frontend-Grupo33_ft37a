cab3b3d66adb9cf2a0c9ab303a6f86e33346ac51
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-07-06 12:08:08 -0300

    deploy

--- components/Menu/Menu.jsx
+++ components/Menu/Menu.jsx
@@ -162,6 +162,12 @@ export default function Menu() {
                         className={`block pb-[0.4rem] font-bold ${pathname === '/about' ? 'border-b-2 border-black' : ''}`}
                     >ABOUT</motion.li>
                 </Link>
+                <Link href={'/search'}>
+                    <motion.li initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.2 }}
+                        whileTap={{ scale: 0.92 }}
+                        className={`block pb-[0.4rem] font-bold ${pathname === '/search' ? 'border-b-2 border-black' : ''}`}
+                    >SHOP</motion.li>
+                </Link>
                 {/* <Link href={'/products/create'}>
                 <motion.li initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.2}}
                     whileTap={{ scale: 0.92}}
