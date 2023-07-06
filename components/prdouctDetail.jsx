5df6bcab88a26ba51250f27a25f995cefc1f3082
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-07-06 13:23:39 -0300

    deploy

--- components/prdouctDetail.jsx
+++ components/prdouctDetail.jsx
@@ -262,7 +262,9 @@ export default function ProductDetail() {
                                             ?
                                             <></>
                                             :
+                                            <div className="flex flex-col gap-y-[0.6rem] mt-[2rem]">
                                             <span className="text-white p-[0.6rem] w-[100%] text-center bg-red-400 font-bold cursor-default">Sold Out</span>
+                                            </div>
                                             :
 
                                             (productDetail[0]?.stock > 0)
