ad70974437e7aa2e215f5759c6ca843cec6aaa3f
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-07-06 06:46:49 -0300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['robohash.org','static.dafiti.com.ar','storage.googleapis.com','www.google.com', 'st2.depositphotos.com']
+        domains:['robohash.org','static.dafiti.com.ar','storage.googleapis.com','www.google.com', 'st2.depositphotos.com','i.pinimg.com']
     }
 }
 
